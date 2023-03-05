18c833980e3572f1c577301494ed8cc070645da7
Arekku <jose_alex170702@hotmail.com>
2023-03-04 22:51:51 -0600

    coming soon

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="bgGradient"></div>
+      <img src="img/LOGO TIKTEC.png" alt="logo" className='logo' />
+      <h1>¡YA REGRESAMOS! <br></br> PRÓXIMAMENTE</h1>
+      <p>EN EL ITESCHAM</p>
+    </div>
+  );
+}
+
+export default App;
